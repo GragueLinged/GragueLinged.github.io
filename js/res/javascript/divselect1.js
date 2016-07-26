62b7f76eb140407d7c612509008804399618c454
GragueLights <gouliang@outlook.com>
2016-07-26 10:45:21 +0800

    update

--- js/res/javascript/divselect1.js
+++ js/res/javascript/divselect1.js
@@ -43,6 +43,8 @@ j.divselect = function(divselectid, inputselectid) {
 		console.log(typeof ul.css("display") + ":" + ul.css("display"));
 		console.log(ul);
 		if (ul.css("display") == "none") {
+			j(divselectid + " cite").addClass('divcite');
+			ul.addClass('ulborder');
 			ul.slideDown();
 		} else {
 			ul.slideUp();
@@ -55,90 +57,92 @@ j.divselect = function(divselectid, inputselectid) {
 			inputselect.val(value);
 			selectvalue = inputselect.val();
 			console.log("本次点击的是:" + selectvalue);
-			$(divselectid + " ul").hide();
+			$(divselectid + " ul").slideUp("slow");
+			j(divselectid + " cite").removeClass('divcite');
+			$(divselectid + " ul").removeClass('ulborder');
 			var myDate = new Date;
 			switch (selectvalue) {
 				case '1':
 					myDate = new Date(myDate.getTime());
-					$('#StartDate1').val(myDate.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(myDate.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(myDate.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(myDate.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是今天");
 					break;
 				case '2':
 					myDate.setDate(myDate.getDate() - 1);
-					$('#StartDate1').val(myDate.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(myDate.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(myDate.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(myDate.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是昨天");
 					break;
 				case '3':
 					var WeekFirstDay = new Date(myDate - (myDate.getDay() - 1) * 86400000);
 					var WeekLastDay = new Date((WeekFirstDay / 1000 + 6 * 86400) * 1000);
-					$('#StartDate1').val(WeekFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(WeekLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(WeekFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(WeekLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是本周");
 					break;
 				case '4':
 					var WeekFirstDay = new Date(myDate - (myDate.getDay() - 1) * 86400000);
 					WeekFirstDay.setDate(WeekFirstDay.getDate() - 7);
 					var WeekLastDay = new Date((WeekFirstDay / 1000 + 6 * 86400) * 1000);
-					$('#StartDate1').val(WeekFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(WeekLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(WeekFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(WeekLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是上周");
 					break;
 				case '5':
 					var MonthFirstDay = new Date(myDate.getFullYear(), myDate.getMonth(), 1);
 					var MonthNextFirstDay = new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1);
 					var MonthLastDay = new Date(MonthNextFirstDay - 86400000);
-					$('#StartDate1').val(MonthFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(MonthLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(MonthFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(MonthLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是本月");
 					break;
 				case '6':
 					var MonthFirstDay = new Date(myDate.getFullYear(), myDate.getMonth() - 1, 1);
 					var MonthNextFirstDay = new Date(myDate.getFullYear(), myDate.getMonth(), 1);
 					var MonthLastDay = new Date(MonthNextFirstDay - 86400000);
-					$('#StartDate1').val(MonthFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(MonthLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(MonthFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(MonthLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是上月");
 					break;
 				case '7':
 					var YearFirstDay = new Date(myDate.getFullYear(), 0, 1);
 					var YearLastDay = new Date(myDate.getFullYear(), 11, 31);
-					$('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是今年");
 					break;
 				case '8':
 					var YearFirstDay = new Date(myDate.getFullYear() - 1, 0, 1);
 					var YearLastDay = new Date(myDate.getFullYear() - 1, 11, 31);
-					$('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					$('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					$('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					$('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					console.log("本次点击的是去年");
 					break;
 				case '9':
 					if(0<myDate.getMonth()<=2){
 						var YearFirstDay = new Date(myDate.getFullYear() , 00, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 02, 31);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}else if(2<myDate.getMonth()<=5){
 						var YearFirstDay = new Date(myDate.getFullYear() , 03, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 05, 30);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}else if(5<myDate.getMonth()<=8){
 						var YearFirstDay = new Date(myDate.getFullYear() , 06, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 08, 30);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}else if(8<myDate.getMonth()<=11){
 						var YearFirstDay = new Date(myDate.getFullYear() , 09, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 11, 31);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}
 					break;
@@ -146,26 +150,26 @@ j.divselect = function(divselectid, inputselectid) {
 				    if(0<myDate.getMonth()<=2){
 				    	var YearFirstDay = new Date(myDate.getFullYear()-1 , 09, 01);
 					    var YearLastDay = new Date(myDate.getFullYear()-1, 11, 31);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 				    }else if(2<myDate.getMonth()<=5){
 						var YearFirstDay = new Date(myDate.getFullYear() , 00, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 02, 31);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}else if(5<myDate.getMonth()<=8){
 						var YearFirstDay = new Date(myDate.getFullYear() , 03, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 05, 30);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}else if(8<myDate.getMonth()<=11){
 						var YearFirstDay = new Date(myDate.getFullYear() , 06, 01);
 					    var YearLastDay = new Date(myDate.getFullYear(), 08, 30);
-					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd"));
-					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd"));
+					    $('#StartDate1').val(YearFirstDay.pattern("yyyy-MM-dd HH:mm:ss"));
+					    $('#EndDate1').val(YearLastDay.pattern("yyyy-MM-dd HH:mm:ss"));
 					    console.log(myDate.getMonth());
 					}
 						break;
